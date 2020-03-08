009c51067216bbb93ec134b40120855d755acfe0
drabick <drabick@hawk.iit.edu>
2020-03-07 20:55:21 -0600

    New react tutorial items

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+function testme(){
+  console.log('Function ran in side react');
+};
+
+//ReactDOM.render(
+//  <h1>Hello, world!</h1>,
+//  document.getElementById('root')
+//);
+
+//const name = 'MCP';
+//const element = <h1>Hello, {testme()}</h1>;
+
+
+// Note: this structure is simplified
+const element = (
+  <h1 className="greeting">
+    Hello, world!
+  </h1>
+);
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
